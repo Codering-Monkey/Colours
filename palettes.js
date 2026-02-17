94912f5754ff739ee6b93bd799336332a3a0baba
MaxGa1ea <max.galea@sydstu.catholic.edu.au>
2026-02-17 12:06:30 +1100

    starting on the palette code

---
+++ palettes.js
@@ -0,0 +1,13 @@
+import { id } from "./script"
+import palettes from "./sorted.json" with { type: "json" }
+
+let parent = id("palettes")
+palettes["any"] = []
+Object.values(palettes).forEach((colours) => {
+    for (let i = 0; i < colours.length; i++) {
+        palettes["any"].push(colours[i])
+    }
+})
+
+console.log(palettes["any"]);
+
