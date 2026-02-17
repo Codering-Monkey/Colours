29fa4672b6e7d7284086d525e2495d2306a34ab8
MaxGa1ea <max.galea@sydstu.catholic.edu.au>
2026-02-17 12:59:29 +1100

    added capitalise

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ Storage.prototype.get = function(key) {
 Storage.prototype.set = function(key, value) {
     this.setItem(key, JSON.stringify(value));
 }
+
+export function capitalise(string, allWords=false) {
+	let stringWords
+	if (allWords) {
+		stringWords = string.split(" ")
+	} else {
+		stringWords = [string]
+	}
+	for (let i = 0; i < stringWords.length; i++) {
+		stringWords[i] = stringWords[i][0].toUpperCase() + stringWords[i].slice(1)
+        if (i < stringWords.length - 1) {
+            stringWords[i] += " "
+        }
+	}
+	return arrayToString(stringWords)
+}
