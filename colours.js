2f1dec7fd4621f203c0445c781c7fefcb335c23c
MaxGa1ea <max.galea@sydstu.catholic.edu.au>
2026-02-17 11:48:11 +1100

    colour button rendering

---
+++ colours.js
@@ -0,0 +1,52 @@
+const colour_buttons = ["red", "orange", "yellow", "green", "blue", "pink", "purple", "brown", "black", "white"];
+
+function id(id) {
+    return document.getElementById(id);
+}
+
+Storage.prototype.get = function(key) {
+    return JSON.parse(this.getItem(key))
+}
+
+Storage.prototype.set = function(key, value) {
+    this.setItem(key, JSON.stringify(value));
+}
+
+for (let i = 0; i < colour_buttons.length; i++) {
+    let button = id(colour_buttons[i])
+    button.style.backgroundColor = "color-mix(in srgb, " + colour_buttons[i] + ", white)";
+    button.style.border = "solid 2px color-mix(in srgb, " + colour_buttons[i] + ", black)"
+    button.addEventListener("click", function() { setColour(colour_buttons[i]) })
+}
+let button = id("any")
+const rainbow_colours = ["red", "orange", "yellow", "green", "blue", "purple", "red"]
+let rainbow_string = ""
+for (let i = 0; i < rainbow_colours.length; i++) {
+    rainbow_string += "color-mix(in srgb, " + rainbow_colours[i] + ", white), ";
+}
+button.style.backgroundImage = "conic-gradient(" + rainbow_string.slice(0, -2) + ")";
+button.style.border = "solid 2px black"
+button.addEventListener("click", function() { setColour("any") })
+setColour("any")
+
+for (let i = 0; i < 100; i++) {
+    id("break").appendChild(document.createElement("br"))
+}
+
+let text = document.createElement("p")
+text.textContent = "hi"
+id("break").appendChild(text)
+
+function setColour(colour) {
+    const oldColour = sessionStorage.get("colour")
+    if (oldColour) {
+        if (oldColour === colour) {
+            id(colour).style.cornerShape = "round"
+            colour = "any"
+        } else {
+            id(oldColour).style.cornerShape = "round"
+        }
+    }
+    id(colour).style.cornerShape = "bevel"
+    sessionStorage.set("colour", colour)
+}
