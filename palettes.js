20ff8187fc12e97fcc126034b72c0136c72b90dd
MaxGa1ea <max.galea@sydstu.catholic.edu.au>
2026-02-19 09:28:05 +1100

    does something now

--- palettes.js
+++ palettes.js
@@ -1,4 +1,4 @@
-import { id } from "./script.js"
+import { copy, id, capitalise, hover } from "./script.js"
 import palettes from "./sorted.json" with { type: "json" }
 
 let parent = id("palettes")
@@ -11,5 +11,31 @@ Object.entries(palettes).forEach(([key, value]) => {
     }
 })
 
-console.log(palettes["any"]);
+export async function render_palettes() {
+    let render_data = palettes[sessionStorage.get("colour")]
+    for (let i = 0; i < render_data.length; i++) {
+        let response = await fetch("./json/" + render_data[i] + ".json")
+        let palette_data = await response.json();
+        let container = document.createElement("div")
+        container.className = "palette"
+
+        let title = document.createElement("div")
+        title.className = "title"
+        title.style.backgroundImage = palette_data["--image"]
+        title.style.backgroundSize = palette_data["--image-size"]
+        container.appendChild(title)
+
+        let titleText = document.createElement("h2")
+        titleText.textContent = capitalise(render_data[i])
+        hover(titleText, palette_data["--contrast"])
+        titleText.addEventListener("click", async function () {await copy(window.location.host + "Colours/json/" + render_data[i] + ".json")})
+        title.appendChild(titleText)
+
+        let content = document.createElement("div")
+        content.classList.add = "content";
+        container.appendChild(content)
+
+        let colours = document.createElement("div")
+        colours.classList.add("colours")
+        container.appendChild(colours)
 
