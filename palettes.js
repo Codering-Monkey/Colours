2e9879654bbd2d19b43a1ddeff1a21f574bbd1c6
MaxGa1ea <max.galea@sydstu.catholic.edu.au>
2026-02-19 14:10:09 +1100

    finished colour boxes

--- palettes.js
+++ palettes.js
@@ -47,6 +47,36 @@ export async function render_palettes() {
             let colour_box = document.createElement("div")
             colour_box.style.backgroundColor = palette_data[colour_spaces[k]]
             colours.appendChild(colour_box)
+            colour_box.classList.add("colour_box")
+            colour_box.id = render_data[i] + colour_spaces[k]
+
+            let colour_text = document.createElement("h3")
+            colour_text.textContent = palette_data[colour_spaces[k]]
+            colour_text.id = render_data[i] + colour_spaces[k] + "text"
+            if (colour_spaces[k] === "--contrast") {
+                colour_text.style.color = palette_data["--alt-text"]
+            } else {
+                colour_text.style.color = palette_data["--text"]
+            }
+            colour_text.addEventListener("click", async function () { await copy(palette_data[colour_spaces[k]]) })
+            colour_box.appendChild(colour_text)
+
+            colour_box.addEventListener("mouseenter", async function () {
+                let other_colours = [...colour_spaces]
+                other_colours.splice(other_colours.indexOf(colour_spaces[k]), 1)
+                console.log(other_colours)
+                for (let j = 0; j < other_colours.length; j++) {
+                    id(render_data[i] + other_colours[j]).style.width = "20%"
+                }
+                colour_box.style.width = "40%"
+                colour_text.style.display = "block"
+            })
+            colour_box.addEventListener("mouseleave", async function () {
+                for (let j = 0; j < colour_spaces.length; j++) {
+                    id(render_data[i] + colour_spaces[j]).style.width = "25%"
+                    id(render_data[i] + colour_spaces[k] + "text").style.display = "none"
+                }
+            })
         }
 
         parent.appendChild(container)
